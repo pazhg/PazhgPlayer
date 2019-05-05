2fad6ed7980654b37257724f15c033b3e4a4d809
Kambiz Guity <kg@pazhg.com>
2019-05-05 19:32:05 +0430

    Initial Commit

---
+++ PazhgSoundPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PazhgSoundPlayer
+//
+//  Created by Pazhg on 2/15/1398 AP.
+//  Copyright © 1398 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
