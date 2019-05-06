f46cc752c12541ee8291a04593380e2275cdb0e1
Kambiz Guity <kg@pazhg.com>
2019-05-06 12:31:49 +0430

    Outlets & Actions

--- PazhgSoundPlayer/ViewController.swift
+++ PazhgSoundPlayer/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelRepeat: UILabel!
+    @IBOutlet weak var labelSpeed: UILabel!
+    @IBOutlet weak var labelVolume: UILabel!
+    
+    @IBOutlet weak var segmentAudioFile: UISegmentedControl!
+    
+    @IBOutlet weak var sliderVolume: UISlider!
+    @IBOutlet weak var sliderAudioSetting: UISlider!
+    
+    @IBOutlet weak var progressDuration: UIProgressView!
+    
+    @IBOutlet weak var switchRepeat: UISwitch!
+    
+    @IBOutlet weak var textviewLog: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    //MARK: - Switch
+    @IBAction func switchRepeat(_ sender: UISwitch) {
+    }
+    
+    //MARK: - Sliders
+    @IBAction func sliderVolume(_ sender: UISlider) {
+    }
+    @IBAction func sliderAudioSetting(_ sender: UISlider) {
+    }
+    
+    //MARK: - Buttons
+    @IBAction func stepperSpeed(_ sender: UIStepper) {
+    }
 
+    @IBAction func buttonPrevious(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonPlayPause(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonStop(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonForward(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonNext(_ sender: UIButton) {
+    }
 }
 
